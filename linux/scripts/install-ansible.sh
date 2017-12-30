a4668ca8f54a81e63e6c6870ab5c27127da0f694
Peter Lorenzen <theheatDK@users.noreply.github.com>
2017-12-30 20:24:39 +0100

    .

--- linux/scripts/install-ansible.sh
+++ linux/scripts/install-ansible.sh
@@ -7,15 +7,16 @@ OS_RELEASE=${OS_RELEASE#NAME=}
 OS_RELEASE=${OS_RELEASE//\"}
 
 if [ "$OS_RELEASE" == "Oracle Linux Server" ]; then
-    echo "$OS_RELEASE - Adding ol7_epel"
-    EPEL_EXISTS=$(grep EPEL /etc/yum.repos.d/public-yum-ol7.repo | wc -l)
+    OEL_REPO="/etc/yum.repos.d/public-yum-ol7.repo"
+    EPEL_EXISTS=$(grep "ol7_developer" $OEL_REPO | wc -l)
     if [ "$EPEL_EXISTS" == "0" ]; then
-        echo "[ol7_epel]" >> /etc/yum.repos.d/public-yum-ol7.repo
-        echo "name=EPEL for Oracle Linux 7 (x86_64)" >> /etc/yum.repos.d/public-yum-ol7.repo
-        echo "baseurl=http://yum.oracle.com/repo/OracleLinux/OL7/developer_EPEL/x86_64/" >> /etc/yum.repos.d/public-yum-ol7.repo
-        echo "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle" >> /etc/yum.repos.d/public-yum-ol7.repo
-        echo "gpgcheck=1" >> /etc/yum.repos.d/public-yum-ol7.repo
-        echo "enabled=1" >> /etc/yum.repos.d/public-yum-ol7.repo
+        echo "$OS_RELEASE - Adding ol7_developer to $OEL_REPO"
+        echo "[ol7_developer]"                                                         >> $OEL_REPO
+        echo "name=Oracle Linux $releasever Development Packages ($basearch)"          >> $OEL_REPO
+        echo "baseurl=http://yum.oracle.com/repo/OracleLinux/OL7/developer/$basearch/" >> $OEL_REPO
+        echo "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle"                       >> $OEL_REPO
+        echo "gpgcheck=1"                                                              >> $OEL_REPO
+        echo "enabled=1"                                                               >> $OEL_REPO
     fi
     
 elif [ "$OS_RELEASE" == "CentOS Linux" ]; then
